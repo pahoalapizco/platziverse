845bdd2e37bf5be2d6b15f80099899e865513765
Paola Alapizco <pahoc.alapizco@gmail.com>
2019-11-06 23:29:14 -0700

    Se agrega la la propiedad para autenticar las rutas de la api

--- platziverse-utils/lib/db.js
+++ platziverse-utils/lib/db.js
@@ -3,13 +3,17 @@ const defaults = require('defaults')
 
 const config = (config) => {
   config = defaults(config, {
-    database: process.env.DB_NAME || 'platziverse',
-    username: process.env.DB_USER || 'platzi',
-    password: process.env.DB_PASS || 'platzi',
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'postgres'
-  })
-
+      database: process.env.DB_NAME || 'platziverse',
+      username: process.env.DB_USER || 'platzi',
+      password: process.env.DB_PASS || 'platzi',
+      host: process.env.DB_HOST || 'localhost',
+      dialect: 'postgres'
+    }
+  )
+  config.auth = {
+    secret: process.env.SECRET || 'mySecret'
+  }
+  
   return config
 }
 
