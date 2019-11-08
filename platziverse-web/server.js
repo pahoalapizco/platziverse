8f3b37f27d9c586c34bd56160ce59f7f4b9522df
pahoalapizco <pahoc.alapizco@gmail.com>
2019-11-08 15:23:02 -0700

    Integrando sockets al servedor estatico

--- platziverse-web/server.js
+++ platziverse-web/server.js
@@ -5,6 +5,7 @@ const http = require('http')
 const chalk = require('chalk')
 const express = require('express')
 const path = require('path')
+const socketio = require('socket.io')
 const {
     handleFatalError, 
     handleError
@@ -13,8 +14,22 @@ const {
 const port = process.env.PORT || 8080
 const app = express()
 const server = http.createServer(app)
+const io = socketio(server)
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+// WebSocket
+io.on('connect', socket => {
+    debug(`Connected ${socket.id}`)
+
+    socket.on('agent/message', payload => {
+        console.log(payload)
+    })
+
+    setInterval(() => {
+        socket.emit('agent/message', { agent: 'xxx-yyy'})
+    }, 2000)
+})
 
 server.listen(port, () => {
   console.log(`${chalk.green('[platziverse:web]')} server listening on port ${port}`)
