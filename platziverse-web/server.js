f7ab32da84dac941f54afa86291fcd92fd1652d7
pahoalapizco <pahoc.alapizco@gmail.com>
2019-11-11 15:22:44 -0700

    Integrando el agente de monitoreo con socket.io

--- platziverse-web/server.js
+++ platziverse-web/server.js
@@ -10,11 +10,13 @@ const {
     handleFatalError, 
     handleError
 } = require('platziverse-utils')
+const PlatziverseAgent = require('platziverse-agent')
 
 const port = process.env.PORT || 8080
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
+const agent = new PlatziverseAgent()
 
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -22,15 +24,21 @@ app.use(express.static(path.join(__dirname, 'public')))
 io.on('connect', socket => {
     debug(`Connected ${socket.id}`)
 
-    socket.on('agent/message', payload => {
-        console.log(payload)
-    })
+   agent.on('agent/message', payload => {
+        socket.emit('agent/message', payload)
+   })
 
-    setInterval(() => {
-        socket.emit('agent/message', { agent: 'xxx-yyy'})
-    }, 2000)
+   agent.on('agent/connected', payload => {
+        socket.emit('agent/connected', payload)
+   })
+
+   agent.on('agent/disconnected', payload => {
+        socket.emit('agent/disconnected', payload)
+   })
+
 })
 
 server.listen(port, () => {
   console.log(`${chalk.green('[platziverse:web]')} server listening on port ${port}`)
+  agent.connect()
 })
